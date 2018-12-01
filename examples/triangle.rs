0ff1e32030a4bc517983466cae7846e8ad2fb650
Jeron Lau <jeron.lau@plopgrizzly.com>
2018-12-01 16:19:08 -0600

    Moved into 64x64 image size.

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -5,11 +5,11 @@ use footile::{FillRule,PathBuilder,Plotter};
 
 fn main() -> Result<(), std::io::Error> {
     let path = PathBuilder::new().absolute()
-                           .move_to(0.75 * 84.0, 0.75 * 64.0)
-                           .line_to(0.25 * 84.0, 0.75 * 64.0)
-                           .line_to(0.5 * 84.0, 0.25 * 64.0)
-                           .line_to(0.75 * 84.0, 0.75 * 64.0)
+                           .move_to(53.0, 48.0)
+                           .line_to(11.0, 48.0)
+                           .line_to(32.0, 16.0)
+                           .line_to(53.0, 48.0)
                            .close().build();
-    let mut p = Plotter::new(84, 64);
+    let mut p = Plotter::new(64, 64);
     p.fill(&path, FillRule::NonZero).write_png("./triangle.png")
 }
